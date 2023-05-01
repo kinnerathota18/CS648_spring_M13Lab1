163c3544e3433f4279bde6dce92af1103259a287
kinnerathota18 <89938504+kinnerathota18@users.noreply.github.com>
2023-04-30 21:22:16 -0700

    Update employees.jsx

--- module01/src/employees.jsx
+++ module01/src/employees.jsx
@@ -1,21 +1,21 @@
-let sally = "Sally Smith";
-let mark = "Mark Martin";
-let holly = "Holly Unlikely";
-let amol = "Amol Shookup";
-let jay = "Jay Gujarathi";
-let saurabh = "Saurabh Deshpande";
-let pankaj = "Pankaj Chohan";
-let eren = "Eren chowdhary";
+let adrian = "Adrian Volkov";
+let tony = "Tony Stark";
+let ceceily = "Ceceily Knight";
+let aiden = "Aiden King";
+let alessio = "Alessio Ivanshov";
+let caleb = "Caleb Lockerhart";
+let iabella = "Isabella Ronin";
+let luke = "Luke Taylor";
 const element = (
   <ul style={{ color: "blue", fontSize: "24px" }}>
-    <li>{sally}</li>
-    <li>{mark}</li>
-    <li>{holly}</li>
-    <li>{amol}</li>
-    <li>{jay}</li>
-    <li>{saurabh}</li>
-    <li>{pankaj}</li>
-    <li>{eren}</li>
+    <li>{adrian}</li>
+    <li>{tony}</li>
+    <li>{ceceily}</li>
+    <li>{aiden}</li>
+    <li>{alessio}</li>
+    <li>{caleb}</li>
+    <li>{iabella}</li>
+    <li>{luke}</li>
   </ul>
 );
 ReactDOM.render(element, document.getElementById("content"));
